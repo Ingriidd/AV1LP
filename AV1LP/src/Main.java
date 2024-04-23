e7a518e2753f012483a452271298a5cd61fd28a4
Ingrid Aparecida <163058193+Ingriidd@users.noreply.github.com>
2024-04-22 22:33:58 -0300

    Add files via upload

---
+++ AV1LP/src/Main.java
@@ -0,0 +1,106 @@
+// AV1 LP - Ingrid - RA 12118641
+
+package BBB24;
+        import java.sql.SQLOutput;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Random;
+        import java.util.Scanner;
+
+        public class CadastroParticipanteBB24 {
+                public static void main(String[] args) {
+                        Scanner scanner = new Scanner(System.in);
+                        Random random = new Random();
+                        List<String> participantes  = new ArrayList<>();
+
+                        System.out.println("Seja bem-vindo a página de cadastros dos brothes do bbb24!");
+
+                                int opcao = scanner.nextInt();
+                                scanner.nextLine();
+
+                                switch (opcao) {
+                                        case 1: // cadastro de participantes
+                                                System.out.println("\nInforme os nomes dos participantes:");
+                                                String nome = scanner.nextLine();
+                                                String participante = new String();
+
+                                                public static void main(String[] args) {
+                                                Scanner ler = new Scanner(System.in);
+                                                ArrayList<String> agenda = new ArrayList();
+                                                participantes.add("Alane");
+                                                participantes.add("Beatriz");
+                                                participantes.add("Davi");
+                                                participantes.add("Fernanda");
+                                                participantes.add("Giovanna Lima");
+                                                participantes.add("Giovanna Pitel");
+                                                participantes.add("Isabelle");
+                                                participantes.add("Juninho");
+                                                participantes.add("Leidy");
+                                                participantes.add("Lucas Henrique");
+                                                participantes.add("Lucas Luigi");
+                                                participantes.add("Lucas Pizane");
+                                                participantes.add("Marcus");
+                                                participantes.add("Matteus");
+                                                participantes.add("Maycon");
+                                                participantes.add("MC Bin Laden");
+                                                participantes.add("Michel");
+                                                participantes.add("Nizam");
+                                                participantes.add("Raquele");
+                                                participantes.add("Rodriguinho");
+                                                participantes.add("Thalyta");
+                                                participantes.add("Vanessa");
+                                                participantes.add("Vinicius");
+                                                participantes.add("Wanessa");
+                                                participantes.add("Yasmin");
+
+                                                System.out.printf("Percorrendo o ArrayList (usando o índice)\n");
+                                                int n = participantes.size();
+
+                                                System.out.println("Participante cadastrado com sucesso!");
+                                                break;
+
+                                        public void incrementaUmVoto() {
+                                                String votos = scanner.nextLine();
+                                                String
+                                        }
+                                                this.votos = getVotos() + 1;
+                                        get.votos = 'S'
+                                        }
+
+                                        while (true) {
+                                                System.out.println("\nSelecione a pessoa que você deseja eliminar:");
+                                                System.out.println("1. Visualizar nome dos participantes emparedados.");
+                                                System.out.println("2. Voto contabilizado com sucesso!");
+                                                System.out.println("3. Votar novamente!");
+                                                System.out.println("5. Sair.");
+
+                                                case 2:
+                                                        int participanteVotado = random.nextInt(101);
+                                                        System.out.println("\nNomeVotado: " + 1);
+                                                        break;
+
+                                                private static void participanteEliminado() {
+                                                        System.out.println("\nNome do participante + quantidade de votos");
+                                                        Scanner in = new Scanner(System.in);
+                                                        char participantes = in.nextInt();
+                                                        participante = 'X'
+                                                        if(nomeParticipante % 2 == 0){
+                                                                System.out.println("Se eu conseguir mover montanhas, se eu conseguir surfar um tsunami, " +
+                                                                        "se eu conseguir\n" +
+                                                                        "domar o sol, se eu conseguir fazer o mar virar sertão, e o sertão virar mar, se eu\n" +
+                                                                        "conseguir dizer o que eu nunca vou conseguir dizer, aí terá chegado o dia em que eu\n" +
+                                                                        "vou conseguir te eliminar com alegria. Com **'S** votos, é você quem sai\n" +
+                                                                        "**Participante X**”.");
+                                                        }else{
+
+                                                        }
+                                                        }
+
+
+
+
+
+
+
+
+
